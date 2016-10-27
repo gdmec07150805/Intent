98fa0e9a0d21955f74d2bb4a7582cc1cd49cb7e5
gdmec07150805 <1395243186@qq.com>
2016-10-27 12:47:30 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150805/intent/IntentDemo3.java
@@ -0,0 +1,9 @@
+package cn.edu.gdmec.s07150805.intent;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JMC on 2016/10/25.
+ */
+public class IntentDemo3  extends AppCompatActivity {
+}
